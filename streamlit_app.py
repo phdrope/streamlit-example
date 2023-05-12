2057ba3b7bc9c831e4bf0a8f5d1a0281e749c460
phdrope <68755711+phdrope@users.noreply.github.com>
2023-05-12 12:01:21 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,103 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+tipoSolicitacao = st.selectbox(
+    'Tipo de Solicição:',
+    (
+    '...',
+    'Integração de Pedidos',
+    'Integração Vários Pedidos',
+    'Pré-pagamento',
+    'Alterações via Update - Permitir faturar s/ conciliar',
+    'Alterações via Update - Taxa Adm, Markup, FEE, Comissão, Outras Taxas',
+    'Alterações via Update - Agente, Cliente, Consolidador, Fornecedor',
+    'Alterar Origem do Pedido',
+    'Cancelar / Emitir Notas Fiscais - NF',
+    'Cancelar, Excluir Pedidos ou Duplicidade',
+    'Erro Faturas / Lotes',
+    'Erro/Problema Portal de Faturas',
+    'VM WEB fora / WEBTES fora (Sem Conexão)',
+    'Instalação do Sistema Benner',
+    'Outras Solicitações' ))
 
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+if tipoSolicitacao == 'Integração de Pedidos':
+    st.write(' ')
+    st.write('Para integração de dois ou mais pedidos, utilize a opção "Integração Vários Pedido"')
+    st.number_input('Número do Pedido, OS ou SV: *')
+    st.selectbox('Sistema de Reserva: *', ('...','ARGO','RESERVE', 'LEMONTECH','PAYTRACK','WOOBA'))
+    st.selectbox('Cliente: *', ('Exemplos','Vale','BRF', 'Minerva', 'Kroton - Cogna'))
+    st.text_area("Descrição:")
+
+if tipoSolicitacao == 'Integração Vários Pedidos':
+    st.write(f'''Para a integração de vários pedidos, por gentileza, anexar uma planilha contendo as informações:
+
+1) Número do Pedido, OS ou SV
+2) Nome do Cliente
+3) Sistema de Reserva - OBT
+''')
+
+    st.text_input('Assunto:')
+    st.text_area('Descrição:')
+    st.file_uploader('Anexar a planilha com os pedidos: *')
+    st.checkbox('Está anexada a planilha com as informações solicitadas? *')
+
+if ((tipoSolicitacao == 'Pré-pagamento')
+    or (tipoSolicitacao == 'Outras Solicitações') 
+    or (tipoSolicitacao == 'VM WEB fora / WEBTES fora (Sem Conexão)') 
+    or (tipoSolicitacao == 'Erro/Problema Portal de Faturas')
+    or (tipoSolicitacao == 'Cancelar, Excluir Pedidos ou Duplicidade')
+    or (tipoSolicitacao == 'Erro Faturas / Lotes')
+    ):
+    st.write('Ao fazer a solicitação, detalhe o máximo que conseguir, com: prints, arquivos ou qualquer tipo de evidência')
+    st.text_input('Assunto: *')
+    st.text_area('Descrição: *')
+    st.file_uploader('Anexar um arquivo:')
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+if ((tipoSolicitacao == 'Alterar Origem do Pedido')    
+    or (tipoSolicitacao == 'Alterações via Update - Permitir faturar s/ conciliar') ):
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    st.write('Ao fazer a solicitação, anexar um arquivo de planilha contendo pelo menos o RLOC de localização no Benner')
 
-    points_per_turn = total_points / num_turns
+    st.text_input('Assunto:')
+    st.text_area('Descrição:')
+    st.file_uploader('Anexar um arquivo: *')
+    st.checkbox('Está anexada a planilha com as informações solicitadas? *')
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+if tipoSolicitacao == 'Alterações via Update - Taxa Adm, Markup, FEE, Comissão, Outras Taxas':
+    
+    st.write('Informar exatamente o novo valor do campo. O chamado retornará em caso de solicitação de ajustes por percentuais')
+    st.selectbox('Selecionar o campo a ser alterado:',('Taxa Adm','Markup', 'FEE', 'Comissão', 'Outras Taxas'))
+    st.text_input('Assunto:')
+    st.text_area('Descrição:')
+    st.file_uploader('Anexar um arquivo: *')
+    st.checkbox('Está anexada a planilha com as informações solicitadas? *')
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if tipoSolicitacao == 'Alterações via Update - Agente, Cliente, Consolidador, Fornecedor':
+
+    st.write('Informar o RLOC e para qual BKO alterar.')
+
+    st.selectbox('Selecionar o campo a ser alterado:',('Agente','Cliente', 'Consolidador', 'Fornecedor'))
+    st.text_input('Assunto:')
+    st.text_area('Descrição:')
+    st.file_uploader('Anexar um arquivo:')
+
+if tipoSolicitacao == 'Cancelar / Emitir Notas Fiscais - NF':
+
+    st.selectbox('Solicitação:',('Cancelar', 'Emitir'))
+
+    st.text_input('Ordem de Venda: *',placeholder='Preencha com o número da Ordem de Venda da NF')
+    st.text_input('Nota Fiscal: *', placeholder='Preencha o número da Nota Fiscal')
+    st.text_input('Data de Emissão: *')
+    st.text_input('Valor da NF: *')
+
+
+    st.text_input('Assunto:')
+    st.text_area('Descrição:')
+    st.file_uploader('Anexar um arquivo:')
+
+if tipoSolicitacao == 'Instalação do Sistema Benner':
+
+    st.write(f'''Para instalação do Sistema Benner, encaminhar um chamado para equipe de Infraestrutura, através link: 
+https://chamados.copastur.com.br/support/catalog/items/86''')
